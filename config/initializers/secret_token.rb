b12460fc30a397d2017e537bbe3a6e5ccdc1416a
David Meharg <david@fiercelink.com>
2013-02-23 12:42:56 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd57808cfcafc03e1857a8d255e75dc30c7a150bdb05e1faa63f688e3942f0e87c2b1c1b0e3dbb63319396a33c8787f5ad54707d874cb1cae6de4f0a54772db81'
