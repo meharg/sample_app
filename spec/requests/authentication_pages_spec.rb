61e1e9a0db8a304973c0211acbfc1656b705e9c5
David Meharg <david@fiercelink.com>
2013-03-23 10:18:07 -0500

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -83,7 +83,20 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
       end
-    end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end #"for non-signed-in users"
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
